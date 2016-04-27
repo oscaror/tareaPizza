334c4f3ff5c073e0676d537aecc08992570b6db8
oscaror <Ozz@macbook-pro-de-oscar-5.local>
2016-04-27 14:26:56 -0500

    Initial Commit

---
+++ PizzApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PizzApp
+//
+//  Created by Oscar Ortega on 27/04/16.
+//  Copyright © 2016 Ozzcorp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
